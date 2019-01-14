17a1570541a663deabd8189920dc37ad149526bc
Robert Conde <rconde@broadwaytechnology.com>
2019-01-14 10:50:02 -0500

    No need to output the message twice

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -47,5 +47,4 @@ Write-BoxstarterMessage "winrm setup complete"
 Write-BoxstarterMessage "Installing .NET Framework 3.5"
 Install-WindowsFeature Net-Framework-Core
 
-Write-BoxstarterMessage "Disabling UAC"
 Disable-UAC

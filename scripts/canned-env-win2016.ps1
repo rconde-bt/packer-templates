7046070a89d252054860bdf60b4151385e73c58f
Robert Conde <rconde@broadwaytechnology.com>
2019-09-19 08:31:37 -0400

    Install Chocolatey

--- scripts/canned-env-win2016.ps1
+++ scripts/canned-env-win2016.ps1
@@ -3,3 +3,6 @@ Install-WindowsFeature Net-Framework-Core
 
 Write-Host "disabling LUA"
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA  -Value 0
+
+Write-Host "Installing chocolatey"
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))

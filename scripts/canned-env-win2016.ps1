ce7e59c96b5e8ebdde556739dcefccddc97d3bba
Robert Conde <rconde@broadwaytechnology.com>
2020-03-09 10:34:55 -0400

    New chocolatey installation instructinos

--- scripts/canned-env-win2016.ps1
+++ scripts/canned-env-win2016.ps1
@@ -5,4 +5,4 @@ Write-Host "disabling LUA"
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA  -Value 0
 
 Write-Host "Installing chocolatey"
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
